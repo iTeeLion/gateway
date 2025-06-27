b485b75ffda6a59ad1b83d15da4947a3947a8b90
Sergey Abu <dev@iteelion.ru>
2025-06-27 23:50:55 +0300

    Update gwinit.sh

--- gwinit.sh
+++ gwinit.sh
@@ -1,36 +1,74 @@
 #!/bin/bash
 
-passwd root
+change_root_password() {
+  passwd root
+}
 
-read -p "Hostname: " HOSTNAME
-hostname $HOSTNAME
-echo "$HOSTNAME" > /etc/hostname
+install_packages() {
+  sudo apt update && apt upgrade -y && apt install sudo mc htop
+}
 
-read -p "New user: " USER
-useradd -m -U -G sudo -s /bin/bash $USER
-passwd $USER
-echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USER
+change_hostname() {
+  read -p "Hostname: " HOSTNAME
+  hostname $HOSTNAME
+  echo "$HOSTNAME" > /etc/hostname
+}
 
-mkdir /home/$USER/.ssh
-chmod 700 /home/$USER/.ssh
-chmod -R 600 /home/$USER/.ssh/*
-chown -R $USER:$USER /home/$USER
-read -p -s "SSH key: " SSH_KEY
-echo "$SSH_KEY" > /home/$USER/.ssh/authorized_keys
+add_user() {
+  read -p "New user: " USER
+  useradd -m -U -G sudo -s /bin/bash $USER
+  passwd $USER
+}
 
-read -p "SSH port: " SSH_PORT
-sudo sed -i 's/^#*Port .*/Port $SSH_PORT/' /etc/ssh/sshd_config
-sudo sed -i 's/^#*PermitRootLogin .*/PermitRootLogin no/' /etc/ssh/sshd_config
-systemctl restart sshd
+add_user_key() {
+  mkdir /home/$USER/.ssh
+  chmod 700 /home/$USER/.ssh
+  chmod -R 600 /home/$USER/.ssh/*
+  chown -R $USER:$USER /home/$USER
+  read -p -s "SSH key: " SSH_KEY
+  echo "$SSH_KEY" > /home/$USER/.ssh/authorized_keys
+}
 
-sudo apt update && apt upgrade -y && apt install sudo mc htop
+allow_user_sudo() {
+  echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USER
+}
 
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+setup_ssh_port() {
+  read -p "SSH port: " SSH_PORT
+  sudo sed -i 's/^#*Port .*/Port $SSH_PORT/' /etc/ssh/sshd_config
+}
+
+disable_ssh_root() {
+  sudo sed -i 's/^#*PermitRootLogin .*/PermitRootLogin no/' /etc/ssh/sshd_config
+}
+
+setup_ssh() {
+  setup_ssh_port
+  disable_ssh_root
+  systemctl restart sshd
+}
+
+install_docker() {
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update -y
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+main() {
+  change_root_password
+  install_packages
+  change_hostname
+  add_user
+  add_user_key
+  allow_user_sudo
+  setup_ssh
+  install_docker
+}
+
+main
