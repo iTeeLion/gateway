b6c058c2f4cb31faa409aa65eb81ca8c3c1a01ef
Sergey Abu <dev@iteelion.ru>
2025-06-29 14:50:46 +0300

    Update 3xui.sh

--- 3xui.sh
+++ 3xui.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-apt install -y snapd
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
-certbot certonly --standalone
+ask_hostname() {
+    read -p "Hostname: " HOSTNAME
+    if [ -z "$HOSTNAME" ]; then
+        ask_hostname
+    fi
+}
 
-apt install git
-cd /srv
-git clone https://github.com/MHSanaei/3x-ui.git
-cd /srv/3x-ui
-# ADD: - /etc/letsencrypt:/etc/letsencrypt (TO volumes)
-docker compose up -d
+install_certbot() {
+    apt install -y snapd
+    sudo snap install --classic certbot
+    sudo ln -s /snap/bin/certbot /usr/bin/certbot
+}
 
-# /etc/letsencrypt/live/DOMAINHERE/cert.pem
-# /etc/letsencrypt/live/DOMAINHERE/privkey.pem
+get_3xui() {
+    apt install git
+    cd /srv
+    git clone https://github.com/MHSanaei/3x-ui.git
+    cd /srv/3x-ui
+    sed -i -E "s/^([[:space:]]*)#?[[:space:]]*hostname:[[:space:]]*.+/\1hostname: $HOSTNAME/" docker-compose.yml
+    sed -i "/volumes:/a\      - /etc/letsencrypt:/etc/letsencrypt" docker-compose.yml
+}
+
+run_3xui() {
+    cd /srv/3x-ui
+    docker compose up -d
+}
+
+main() {
+    ask_hostname
+    install_certbot
+    certbot certonly --standalone -d $HOSTNAME
+    get_3xui
+    run_3xui
+}
+main
