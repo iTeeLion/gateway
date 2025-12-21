13fcf3d16892b90bc1c807bd2808a40a2b02f20d
Sergey Abu <dev@iteelion.ru>
2025-12-21 13:37:21 +0300

    Update install_remnawave.sh

--- scripts/install_remnawave.sh
+++ scripts/install_remnawave.sh
@@ -23,6 +23,13 @@ install_remnawave() {
     # docker compose up -d && docker compose logs -f -t
 }
 
+install_npm() {
+    mkdir /opt/npm && cd /opt/npm
+    curl -o docker-compose.yml https://raw.githubusercontent.com/iTeeLion/gateway/refs/heads/main/configs/nginx-proxy-manager/docker-compose.yml
+    curl -o .env https://raw.githubusercontent.com/iTeeLion/gateway/refs/heads/main/configs/nginx-proxy-manager/.env.sample
+    pw=$(openssl rand -hex 24) && sed -i "s/^POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$pw/" .env && sed -i "s|^\(DATABASE_URL=\"postgresql://postgres:\)[^\@]*\(@.*\)|\1$pw\2|" .env
+}
+
 main() {
     update_packages
     install_docker
