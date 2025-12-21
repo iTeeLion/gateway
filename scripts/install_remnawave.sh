fcd7a7887fcf38a7e8135d0bdfc02cabb1e1bdb8
Sergey Abu <dev@iteelion.ru>
2025-12-21 13:20:41 +0300

    Update install_remnawave.sh

--- scripts/install_remnawave.sh
+++ scripts/install_remnawave.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+update_packages() {
+    apt update && apt upgrade -y
+}
+
+install_docker() {
+    sudo curl -fsSL https://get.docker.com | sh
+}
+
+install_remnawave() {
+    echo "ToDo"
+}
+
+main() {
+    update_packages
+    install_docker
+    install_remnawave
+}
+
+main
