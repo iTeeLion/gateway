67b79843044bae925bdfea25322684baa1eafe4f
Sergey Abu <dev@iteelion.ru>
2026-01-06 23:21:10 +0300

    Update install_remnawave.sh

--- scripts/install_remnawave.sh
+++ scripts/install_remnawave.sh
@@ -17,11 +17,6 @@ install_remnawave() {
     sed -i "s/^JWT_AUTH_SECRET=.*/JWT_AUTH_SECRET=$(openssl rand -hex 64)/" .env && sed -i "s/^JWT_API_TOKENS_SECRET=.*/JWT_API_TOKENS_SECRET=$(openssl rand -hex 64)/" .env
     sed -i "s/^METRICS_PASS=.*/METRICS_PASS=$(openssl rand -hex 64)/" .env && sed -i "s/^WEBHOOK_SECRET_HEADER=.*/WEBHOOK_SECRET_HEADER=$(openssl rand -hex 64)/" .env
     pw=$(openssl rand -hex 24) && sed -i "s/^POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$pw/" .env && sed -i "s|^\(DATABASE_URL=\"postgresql://postgres:\)[^\@]*\(@.*\)|\1$pw\2|" .env
-    
-    docker network create proxy-network
-
-    # ToDo
-    # docker compose up -d && docker compose logs -f -t
 }
 
 install_npm() {
@@ -33,11 +28,23 @@ install_npm() {
     pw=$(openssl rand -hex 24) && sed -i "s/^POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$pw/" .env && sed -i "s|^\(DATABASE_URL=\"postgresql://postgres:\)[^\@]*\(@.*\)|\1$pw\2|" .env
 }
 
+message() {
+    echo "finish .env filling and run:"
+    echo "cd /opt/remnawave && docker compose up -d && docker compose logs -f -t"
+}
+
 main() {
     update_packages
+    
     install_docker
+
+    docker network create proxy-network
+    
     install_remnawave
+    
     install_npm
+
+    message
 }
 
 main
