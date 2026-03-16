86cd5b5d82c8273df87a068e693e5051a49e7a24
Sergey Abu <dev@iteelion.ru>
2026-03-16 16:38:53 +0300

    Update mtproto.sh

--- scripts/mtproto.sh
+++ scripts/mtproto.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+mkdir /opt/mtproto
+
+cd /opt/mtproto
+
+curl -o .env https://raw.githubusercontent.com/iTeeLion/gateway/refs/heads/main/configs/mtproto/docker-compose.yml
+curl -o .env https://raw.githubusercontent.com/iTeeLion/gateway/refs/heads/main/configs/mtproto/config.env
